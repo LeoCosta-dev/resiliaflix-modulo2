23d21e2ab5f2d301f7c2de123e35f47f1aadace4
LeoCosta-Dev <leonardodacostadasilva@hotmail.com>
2021-09-13 18:47:12 -0300

    validações de cep feitas

--- cep.js
+++ cep.js
@@ -8,22 +8,41 @@ cep.addEventListener("blur", () => {
         document.getElementById("uf").value = ""
         document.getElementById("bairro").value = ""
     }
+    else if(validaCep(cep) == "inválido"){
+       return cepInvalido(cep)
+    }
     else {
         return $.ajax({
 
-            url: "https://viacep.com.br/ws/" + cep.value + "/json/",
+            url: "https://viacep.com.br/ws/" + validaCep(cep) + "/json/",
             method: "get",
-            success: function (response) {
-                document.getElementById("rua").value = response.logradouro
-                document.getElementById("cidade").value = response.localidade
-                document.getElementById("uf").value = response.uf
-                document.getElementById("bairro").value = response.bairro
-                console.log(response)
+            success: function (resposta) {
+                autoComplete(resposta)
             }
+
+
         }
         )
     }
 })
+
+function autoComplete(response){
+    if(response.logradouro){
+                document.getElementById("rua").value = response.logradouro
+                document.getElementById("cidade").value = response.localidade
+                document.getElementById("uf").value = response.uf
+                document.getElementById("bairro").value = response.bairro
+                cep.style.borderColor = "blue"
+                console.log(response)
+    }else{
+        document.getElementById("rua").value = ""
+        document.getElementById("cidade").value = ""
+        document.getElementById("uf").value = ""
+        document.getElementById("bairro").value = ""
+        cep.style.borderColor = "red"
+        alert('Cep não encontrado.')
+    }
+}
 
 reset = document.createElement("script")
 reset.textContent = `cep.value=""
