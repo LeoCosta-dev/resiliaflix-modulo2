2996e033796aa7ba91fa7df3a03ff0f93415a5df
MuriloMininel <murilomininel1@hotmail.com>
2021-09-13 16:03:09 -0300

    reset do formulario

--- cep.js
+++ cep.js
@@ -1,17 +1,25 @@
 let cep = document.getElementById("cep")
 
 cep.addEventListener("blur", () => {
-  return  $.ajax({
+    if (cep.value == "") {
+        document.getElementById("rua").value =""
+        document.getElementById("cidade").value = ""
+        document.getElementById("uf").value = ""
+        document.getElementById("bairro").value = ""
+    }
+    else {
+        return $.ajax({
 
-        url: "https://viacep.com.br/ws/" + cep.value + "/json/",
-        method: "get",
-        success: function (response) {
-            document.getElementById("rua").value=response.logradouro
-            document.getElementById("cidade").value=response.localidade
-            document.getElementById("uf").value=response.uf
-            document.getElementById("bairro").value=response.bairro
-            console.log(response)
+            url: "https://viacep.com.br/ws/" + cep.value + "/json/",
+            method: "get",
+            success: function (response) {
+                document.getElementById("rua").value = response.logradouro
+                document.getElementById("cidade").value = response.localidade
+                document.getElementById("uf").value = response.uf
+                document.getElementById("bairro").value = response.bairro
+                console.log(response)
+            }
         }
-    });
+        )
+    }
 })
-
