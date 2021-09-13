f946d57bbf44721a0bbb7588b106de205f1bf7d7
MuriloMininel <murilomininel1@hotmail.com>
2021-09-13 15:34:30 -0300

    autocomplete completado

--- cep.js
+++ cep.js
@@ -6,7 +6,12 @@ cep.addEventListener("blur", () => {
         url: "https://viacep.com.br/ws/" + cep.value + "/json/",
         method: "get",
         success: function (response) {
+            document.getElementById("rua").value=response.logradouro
+            document.getElementById("cidade").value=response.localidade
+            document.getElementById("uf").value=response.uf
+            document.getElementById("bairro").value=response.bairro
             console.log(response)
         }
     });
 })
+
