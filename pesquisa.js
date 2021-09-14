b3bd5fe363b7a9fa3e3e7694ec320047a7acaf92
Alexandre <prudenteale1@gmail.com>
2021-09-14 02:39:19 -0300

    commit de entrada

--- pesquisa.js
+++ pesquisa.js
@@ -1,3 +1,4 @@
+Catalogo=["O Auto da Compadecida","Batman Dark Knight","The Godfather","The Pianist","Trainspotting","Hannibal","Invictus","2 Coelhos",""]
 $.ajax({
     
     url: "http://www.omdbapi.com/?i=tt0271383&apikey=e1026f57",
