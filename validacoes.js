8b8a83e624c53377e4fd7e232783e16ee80491b9
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-09-17 02:59:10 -0300

    validação de e-mail sem conflitos em outras páginas

--- validacoes.js
+++ validacoes.js
@@ -20,33 +20,34 @@ if (document.getElementById("cep")) {
 }
 
 const inputEmail = document.getElementById('email')
-
-document.getElementById('recupera').addEventListener('click', (event) =>  {
-    event.preventDefault()
-    let validaEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
-    if (validaEmail.test(inputEmail.value)) {
-        let mensagem = document.createElement('p')
-        mensagem.textContent = 'Link de recuperação enviado para e-mail cadastrado'
-        mensagem.setAttribute('id', 'confirma')
-        if(!document.getElementById('confirma')){
-            document.getElementById('box').appendChild(mensagem)
-            setTimeout(() => {
-                mensagem.remove()
-            }, 3000)
+if(inputEmail){
+    document.getElementById('recupera').addEventListener('click', (event) =>  {
+        event.preventDefault()
+        let validaEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+        if (validaEmail.test(inputEmail.value)) {
+            let mensagem = document.createElement('p')
+            mensagem.textContent = 'Link de recuperação enviado para e-mail cadastrado'
+            mensagem.setAttribute('id', 'confirma')
+            if(!document.getElementById('confirma')){
+                document.getElementById('box').appendChild(mensagem)
+                setTimeout(() => {
+                    mensagem.remove()
+                }, 3000)
+            }
+            inputEmail.value = ''
+        }else{
+            let mensagem = document.createElement('p')
+            mensagem.textContent = 'E-mail inválido'
+            mensagem.setAttribute('id', 'naoconfirma')
+            mensagem.style.color = 'red'
+            mensagem.style.borderColor = 'red'
+            if(!document.getElementById('naoconfirma')){
+                document.getElementById('box').appendChild(mensagem)
+                setTimeout(() => {
+                    mensagem.remove()
+                }, 3000)
+            }
+            inputEmail.value = ''
         }
-        inputEmail.value = ''
-    }else{
-        let mensagem = document.createElement('p')
-        mensagem.textContent = 'E-mail inválido'
-        mensagem.setAttribute('id', 'naoconfirma')
-        mensagem.style.color = 'red'
-        mensagem.style.borderColor = 'red'
-        if(!document.getElementById('naoconfirma')){
-            document.getElementById('box').appendChild(mensagem)
-            setTimeout(() => {
-                mensagem.remove()
-            }, 3000)
-        }
-        inputEmail.value = ''
-    }
-})
+    })
+}
