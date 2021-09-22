65404f2c5e67b795a79dcb8df9d0d19cff52bdb4
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-09-22 20:05:42 -0300

    adaptação para github pages

--- JS/eventos.js
+++ JS/eventos.js
@@ -1,21 +1,21 @@
 document.getElementById('home').addEventListener('click', ()=>{
-    location.replace('index.html')
+    location.replace('./index.html')
 })
 document.getElementById('filmes').addEventListener('click', ()=>{
-    location.replace('pesquisa.html')
+    location.replace('/HTML/pesquisa.html')
 })
 document.getElementById('assine').addEventListener('click', ()=>{
-    location.replace('assineja.html')
+    location.replace('/HTML/assineja.html')
 })
 document.getElementById('contato').addEventListener('click', ()=>{
-    location.replace('contato.html')
+    location.replace('/HTML/contato.html')
 })
 document.querySelector('.logo').addEventListener('click', ()=>{
-    location.replace('index.html')
+    location.replace('./index.html')
 })
 document.querySelector('#login').addEventListener('click', ()=>{
     document.querySelector('.ocontainer').style.display = 'flex'
 })
 document.querySelector('#redirecionar').addEventListener('click', ()=>{
-    location.replace('pesquisa.html')
+    location.replace('/HTML/pesquisa.html')
 })
