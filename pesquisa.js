7f3d5909e1a939f5666bcf4f452bb3918ca97dd2
Alexandre <prudenteale1@gmail.com>
2021-09-22 15:11:19 -0300

    Modal aparece e está dinamico, só falta completar

--- pesquisa.js
+++ pesquisa.js
@@ -2,6 +2,18 @@
 let imagem=document.querySelector('#imagem')
 let botao=document.querySelector('#buton')
 let texto=document.querySelector('#input')
+// dom dos elementos do post
+let gender=document.querySelector('#gender')
+let year=document.querySelector('#year')
+let runtime=document.querySelector('#runtime')
+let director=document.querySelector('#director')
+let plot=document.querySelector('#plot')
+let title=document.querySelector('#title')
+let imgpost=document.querySelector('#imgpost')
+let close=document.querySelector('#close-model')
+let closei=document.querySelector('#close-model-i')
+let staticmodal=document.querySelector('#modal')
+
 
 //ao pesquisar um título apenas ele centraliza o titulo e quebra a linha, nao busqui alguma forma de resolver ainda, acho que é coisa do bootstrap que coloquei
 
@@ -51,10 +63,18 @@ function mostrar(objeto){
                 <div class='well text-center'>
                 <div id="erro" >Imagem não encontrada</div>
                 <h5>${objeto.Title}</h5> 
-                <p><button type="button" class="btn btn-primary" id='buton'>Detalhes</button></p>
+                <p><button
+      onclick="add_modal('${objeto.imdbID}')"
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop"
+      >
+        Launch static backdrop modal
+      </button></p>
                 </div>
                 </div>`
-                // botao.addEventListener('click',modal(objeto.imdbID))
+                // botao.addEventListener('click',addmodal2(objeto.imdbID))
                 }
                     
                
@@ -62,16 +82,24 @@ function mostrar(objeto){
                   console.log(objeto.imdbID)
                 
                                  
-                // imagem.innerHTML+=`
-                // <div class="col-md-3">
-                // <div class='well text-center'>
-                // <img src="${objeto.Poster}" alt="${objeto.Title}" height='300'>
-                // <h5>${objeto.Title}</h5>                                    
-                // <p><button type="button" class="btn btn-primary" id='buton' onClick='modal(objeto.imdbID)'>Detalhes</button></p>
-                // </div>`
+                imagem.innerHTML+=`
+                <div class="col-md-3">
+                <div class='well text-center'>
+                <img src="${objeto.Poster}" alt="${objeto.Title}" height='300'>
+                <h5>${objeto.Title}</h5>                                    
+                <p><button
+      onclick="add_modal('${objeto.imdbID}')"
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop"
+      >
+        Launch static backdrop modal
+      </button></p>
+                </div>`
                 
-                modal(objeto.imdbID)
-                  // botao.addEventListener('click',modal(objeto.imdbID))
+                // modal(objeto.imdbID)
+                  // botao.addEventListener('click',addmodal2(objeto.imdbID))
                 }
             }
             
@@ -96,34 +124,31 @@ function modal(param){
             <p><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
             Detalhes
             </button></p>
-            
-            <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document" >
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">${response.Title}</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <img src="${response.Poster}" alt="${response.Title}" height='200'>
+            <div class="modal fade" id="myModal" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">${response.Title}</h4>
+            </div>
+            <div class="modal-body">
+              <img src="${response.Poster}" alt="${response.Title}" height='200'>
       <p>Ano: ${response.Year}</p>
       <p>Duração: ${response.Runtime}</p>
       <p>Gênero: ${response.Genre}</p>
       <p>Diretor: ${response.Director}</p>
       <p>Sinopse: ${response.Plot}</p>
-      
-      </div>
-      <div class="modal-footer">
-    <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
-        
-      </div>
-    </div>
-  </div>
-</div>
             </div>
-            </div>`
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+       </div>
+  </div>');
+            
+            `
 
     
             
@@ -133,4 +158,38 @@ function modal(param){
            
                     })
 
+}
+
+
+function add_modal(param){
+
+  $.ajax({
+          
+      url: `https://www.omdbapi.com/?i=${param}&apikey=82aa4d9d`,
+      method: "post",
+      success: (response)=>{
+        console.log(response)
+                year.innerHTML=response.Year
+                gender.innerHTML=response.Genre
+
+
+ 
+
+
+
+
+
+    
+            
+            
+
+            }
+           
+                    })
+                    staticmodal.style.display='block'
+
+}
+
+function closemodel(){
+  staticmodal.style.display='none'
 }
