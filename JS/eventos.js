e323a3afee5bd48adff549c63672ee384ee0248c
Leonardo da Costa <leonardodacosta@pop-os.localdomain>
2021-09-23 19:14:05 -0300

    home parcialmente resolvido

--- JS/eventos.js
+++ JS/eventos.js
@@ -1,13 +1,8 @@
+if(document.querySelector('#redirecionar')){
 
-document.querySelector('#redirecionar').addEventListener('click', ()=>{
-    location.replace('../HTML/pesquisa.html')
-})
+    document.querySelector('#redirecionar').addEventListener('click', ()=>{
+        location.replace('../HTML/pesquisa.html')
+    })
+}
 
-document.querySelector('.slide').addEventListener('click', ()=>{
-    if(document.querySelector('.poster').style.display = 'none'){
-        document.querySelector('.poster').style.display = 'flex'
-    }
-    else if(document.querySelector('.poster').style.display = 'flex'){
-        document.querySelector('.poster').style.display = 'none'
-    }
-})
+
