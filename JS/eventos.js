68fc4f19044dc0cb552f6ce45cc784730d61bed1
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-09-22 23:16:34 -0300

    jesus

--- JS/eventos.js
+++ JS/eventos.js
@@ -2,13 +2,13 @@ document.getElementById('home').addEventListener('click', ()=>{
     location.replace('./index.html')
 })
 document.getElementById('filmes').addEventListener('click', ()=>{
-    location.replace('/HTML/pesquisa.html')
+    location.replace('./HTML/pesquisa.html')
 })
 document.getElementById('assine').addEventListener('click', ()=>{
-    location.replace('/HTML/assineja.html')
+    location.replace('./HTML/assineja.html')
 })
 document.getElementById('contato').addEventListener('click', ()=>{
-    location.replace('/HTML/contato.html')
+    location.replace('./HTML/contato.html')
 })
 document.querySelector('.logo').addEventListener('click', ()=>{
     location.replace('./index.html')
@@ -17,5 +17,5 @@ document.querySelector('#login').addEventListener('click', ()=>{
     document.querySelector('.ocontainer').style.display = 'flex'
 })
 document.querySelector('#redirecionar').addEventListener('click', ()=>{
-    location.replace('/HTML/pesquisa.html')
+    location.replace('./HTML/pesquisa.html')
 })
