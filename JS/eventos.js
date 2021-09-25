e5bee81893e75a1f88acac298f91c1dd800b6718
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-09-25 13:38:11 -0300

    validaçao de e-mail pronta

--- JS/eventos.js
+++ JS/eventos.js
@@ -1,3 +1,5 @@
+let inputEmail = document.getElementById('email')
+
 if(document.querySelector('#redirecionar')){
 
     document.querySelector('#redirecionar').addEventListener('click', ()=>{
@@ -13,30 +15,59 @@ if(document.getElementById('password2')){
 
 if(document.getElementById('email')){
 
-    const inputEmail = document.getElementById('email')
     if (inputEmail && document.getElementById('recupera')) {
-        document.getElementById('recupera').addEventListener('click', botaoEnviar(Event))
+        document.getElementById('recupera').addEventListener('click', (event)=>{
+            event.preventDefault()
+            let validaEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+            if (validaEmail.test(inputEmail.value)) {
+                let mensagem = document.createElement('p')
+                mensagem.textContent = 'Link de recuperação enviado para e-mail cadastrado'
+                mensagem.style.color = 'green'
+                mensagem.setAttribute('id', 'confirma')
+                if (!document.getElementById('confirma')) {
+                    document.getElementById('box').appendChild(mensagem)
+                    setTimeout(() => {
+                        mensagem.remove()
+                    }, 3000)
+                }
+                inputEmail.value = ''
+            } else {
+                let mensagem = document.createElement('p')
+                mensagem.textContent = 'E-mail inválido'
+                mensagem.setAttribute('id', 'naoconfirma')
+                mensagem.style.color = 'red'
+                mensagem.style.borderColor = 'red'
+                if (!document.getElementById('naoconfirma')) {
+                    document.getElementById('box').appendChild(mensagem)
+                    setTimeout(() => {
+                        mensagem.remove()
+                    }, 3000)
+                }
+                inputEmail.value = ''
+            }})
     }
     
-    if(inputEmail && document.getElementById("cep")){
-        inputEmail.addEventListener('keyup', ()=>{
+    if(document.getElementById("cep")){
+        inputEmail.addEventListener('blur', ()=>{
             
             let validaEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
-            switch(inputEmail){
-                case (validaEmail.test(inputEmail.value)):
-                    inputEmail.style.color = "green"
-                case(inputEmail == ''):
-                    inputEmail.style.color = "black"
-                case(validaEmail.test(inputEmail.value) == false):
-                    inputEmail.style.color = "red"
-            }
+            
+                if(validaEmail.test(inputEmail.value)){
+                    console.log('paçoca');
+
+                    inputEmail.style.border = 'green 2px solid'
+                }
+                else if(inputEmail.value == ''){
+
+                    inputEmail.style.border = 'none'
+                }
+                else{
+
+                    inputEmail.style.border = 'red 2px solid'
+                }
+                
+                
         })
     }
 }
 
-if(document.getElementById('botao')){
-    document.getElementById('botao').addEventListener('click', (event)=>{
-        event.preventDefault
-        validaForms()
-    })
-}
--- JS/validacoes.js
+++ JS/validacoes.js
@@ -38,89 +38,11 @@ function senha(){
             console.log("paçoca");
             document.getElementById('password2').style.borderColor = 'red'
             document.getElementById('password2').style.color = 'red' 
-        }else if(document.getElementById('password2').value === '' || document.getElementById('password').value === ''){
-            console.log("paçoca");
-            semConfirmar()
         }
     }
 
    
 }
-function semConfirmar(){
-    if(document.getElementById('password2').value === '' ){
-        document.getElementById('password').style.borderColor = 'black'
-        document.getElementById('password').style.color = 'black'
-        document.getElementById('password2').style.borderColor = 'red'
-        document.getElementById('password2').style.color = 'red'   
-    }else if(document.getElementById('password').value === '' ){
-        document.getElementById('password2').style.borderColor = 'black'
-        document.getElementById('password2').style.color = 'black'
-        document.getElementById('password').style.borderColor = 'red'
-        document.getElementById('password').style.color = 'red'   
-    }
-}
-function botaoEnviar(parametro){
-    parametro.preventDefault()
-    let validaEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
-    if (validaEmail.test(inputEmail.value)) {
-        let mensagem = document.createElement('p')
-        mensagem.textContent = 'Link de recuperação enviado para e-mail cadastrado'
-        mensagem.style.color = 'green'
-        mensagem.setAttribute('id', 'confirma')
-        if (!document.getElementById('confirma')) {
-            document.getElementById('box').appendChild(mensagem)
-            setTimeout(() => {
-                mensagem.remove()
-            }, 3000)
-        }
-        inputEmail.value = ''
-    } else {
-        let mensagem = document.createElement('p')
-        mensagem.textContent = 'E-mail inválido'
-        mensagem.setAttribute('id', 'naoconfirma')
-        mensagem.style.color = 'red'
-        mensagem.style.borderColor = 'red'
-        if (!document.getElementById('naoconfirma')) {
-            document.getElementById('box').appendChild(mensagem)
-            setTimeout(() => {
-                mensagem.remove()
-            }, 3000)
-        }
-        inputEmail.value = ''
-    }
-}
 
-function validaForms(){
-    let campos = [document.querySelector('#nome').value, 
-    document.querySelector('#cep').value, 
-    document.querySelector('#numero').value, 
-    document.querySelector('#rg').value, 
-    document.querySelector('#email').value, 
-   document.querySelector('#password').value, 
-   document.querySelector('#password2').value]
 
-    for(let i = 0; i > campos.length; i++){
-        if(campos[i] == ''){
-            if(!document.getElementById('formula')){
-                document.getElementById('form-inline').innerHTML= `
-                <a id = "formula" href = "pesquisa.html"><p> Cadastro realizado com sucesso! Clique aqui para esccolher seu filme! </p></a>
-                `
-                document.getElementById('formula').style.color = 'green'
-                if(document.getElementById('formula2')){
-                    document.getElementById('formula2').remove
-                }
-            }
-        }else{
-            if(!document.getElementById('formula2')){
-                document.getElementById('form-inline').innerHTML= `
-                <a id = "formula2" href = "pesquisa.html"><p> Cadastro incompleto! Reveja o formulário! </p></a>
-                `
-                document.getElementById('formula2').style.color = 'red'
-                if(document.getElementById('formula')){
-                    document.getElementById('formula').remove
-                }
-            }
-        }
-    }
-    
-}
+
