12866cecb22d4e7f9e99574162fdb7407b3fcc7a
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-09-25 12:42:08 -0300

    valida senha

--- JS/eventos.js
+++ JS/eventos.js
@@ -5,4 +5,38 @@ if(document.querySelector('#redirecionar')){
     })
 }
 
+if(document.getElementById('password2')){
+    document.getElementById('password2').addEventListener('keyup', ()=>{
+        senha()
+    })
+}
 
+if(document.getElementById('email')){
+
+    const inputEmail = document.getElementById('email')
+    if (inputEmail && document.getElementById('recupera')) {
+        document.getElementById('recupera').addEventListener('click', botaoEnviar(Event))
+    }
+    
+    if(inputEmail && document.getElementById("cep")){
+        inputEmail.addEventListener('keyup', ()=>{
+            
+            let validaEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+            switch(inputEmail){
+                case (validaEmail.test(inputEmail.value)):
+                    inputEmail.style.color = "green"
+                case(inputEmail == ''):
+                    inputEmail.style.color = "black"
+                case(validaEmail.test(inputEmail.value) == false):
+                    inputEmail.style.color = "red"
+            }
+        })
+    }
+}
+
+if(document.getElementById('botao')){
+    document.getElementById('botao').addEventListener('click', (event)=>{
+        event.preventDefault
+        validaForms()
+    })
+}
